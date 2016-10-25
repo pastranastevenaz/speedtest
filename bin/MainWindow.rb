b54db9446b992d55797a1b2b1d9a148b09577deb
Steven Antonio <pastrana.steven.az@gmail.com>
2016-10-25 00:23:46 -0700

    caught exceptions for network request and linked button to destroy application.

--- bin/MainWindow.rb
+++ bin/MainWindow.rb
@@ -1,10 +1,45 @@
+require 'net/http'
+require 'uri'
+
 class MainWindow
 
   include GladeGUI
 
+
   def initialize() #window1_show('args')
-    @status_image = "bin/no_connection.png"
-    @close_image = "bin/Button-Close-icon.png"
+  #  @run = true
+  #  while @run
+    ping
+  #  sleep 10
+  end
+
+
+
+  def close_pressed
+    MainWindow.destroy
+  end
+
+
+  def ping
+    begin
+      url = URI.parse("http://www.stevenanton.io")
+      start_time = Time.now
+      response = Net::HTTP.get(url)
+      end_time = Time.now - start_time
+
+
+
+      if response==""
+        @status_image = "bin/no_connection.png"
+      else
+        @status_image = "bin/good_connection.png"
+      end
+
+      @close_image = "bin/Button-Close-icon.png"
+
+    rescue StandardError
+      @status_image = "bin/no_connection.png"
+    end
   end
 
 end
