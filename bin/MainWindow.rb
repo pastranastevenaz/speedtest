0cf45b812e9359ae78d14250fbbeffc59b2d586c
Steven Antonio <pastrana.steven.az@gmail.com>
2016-10-24 20:36:47 -0700

    moved to visual ruby

---
+++ bin/MainWindow.rb
@@ -0,0 +1,10 @@
+class MainWindow
+
+  include GladeGUI
+
+  def initialize() #window1_show('args')
+    @status_image = "bin/no_connection.png"
+    @close_image = "bin/Button-Close-icon.png"
+  end
+
+end
---
+++ main.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+
+# ignore -- this is for development, same as require 'vrlib'
+require File.exists?("./../../vrlib/vrlib.rb") ?  "./../../vrlib/vrlib.rb" : "vrlib"
+
+# from require_all gem:
+require_rel 'bin/'
+
+
+MainWindow.new.show_glade()
